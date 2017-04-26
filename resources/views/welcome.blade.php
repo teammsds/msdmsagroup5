b5a1c6e0844adb060d2d48740e69dfdca20f2fed
teammsds <teammsd70@gmail.com>
2017-04-26 09:26:48 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,11 +24,11 @@
         </div>
         <div class="col-lg-8 col-md-8 wow bounceInRight">
                 <i class="fa fa-futbol-o" style="font-size: 40px;color:red"></i>
-           <a style= "color: white;text-shadow: 2px 2px 4px #000000" href="{{ action('MatchController@index')}}" class="glow"><h2 style= "color: red ;font-size: 30px">Visit ongoing matches</h2></a>
+           <a style= "color: white;text-shadow: 2px 2px 4px #000000" href="{{ action('MatchController@index')}}" class="glow"><h2 style= "color: red ;font-size: 20px">Visit ongoing matches</h2></a>
            <br>
            <br>
            <i class="fa fa-futbol-o" style="font-size: 40px;color:Green"></i>
-           <a style= "color: white;text-shadow: 2px 2px 4px #000000" href="{{ action('TournamentController@index')}}" class="glow"><h2 style= "color: red ;font-size: 30px">Tournament List</h2></a>
+           <a style= "color: white;text-shadow: 2px 2px 4px #000000" href="{{ action('TournamentController@index')}}" class="glow"><h2 style= "color: red ;font-size: 20px">Tournament List</h2></a>
              </div>
           <div class="col-md-4 col-md-offset-2">
             
